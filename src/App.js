99f97a6413aa3d38349feec59a6d233b05853732
Richard Hennessey III <Rick@Richards-iMac.local>
2020-06-17 14:04:29 -0400

    useref hook working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react"
 
 function App() {
+  const [newTodoValue, setNewTodoValue] = useState("")
+  const [todosList, setTodosList] = useState([])
+  const inputRef = useRef()
+
+  function handleChange(event) {
+    setNewTodoValue(event.target.value)
+  }
+
+  function addTodo(event) {
+    event.preventDefault()
+    setTodosList(prevTodosList => [...prevTodosList, newTodoValue])
+    setNewTodoValue("")
+    inputRef.current.focus()
+  }
+
+  const allTodos = todosList.map(todo => <p key={todo}>{todo}</p>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input ref={inputRef} type="text" name="todo" value={newTodoValue} onChange={handleChange} />
+        <button onClick={addTodo}>Add todo item</button>
+      </form>
+      {allTodos}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
